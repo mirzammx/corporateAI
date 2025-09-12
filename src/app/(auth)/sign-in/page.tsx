fdf5d8fbdd44207ef4901c0ab735c3f23b722bca
mohammed mirza <1ds22cb032@dsce.edu.in>
2025-09-12 16:25:03 +0530

    05:authentication socials

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,5 +1,14 @@
 import { SignInview } from "@/modules/auth/ui/views/sign-in-view";
-const Page=()=>{
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+const Page=async()=>{
+    const session=await auth.api.getSession({
+        headers:await headers(),
+      });
+      if(!!session){
+        redirect("/");
+      }
     return <SignInview/>
 }
 export default Page;
--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -1,6 +1,6 @@
 "use client";
 import{Card,CardContent} from "@/components/ui/card";
-import{email, file, z}from"zod";
+import{ z}from"zod";
 import { OctagonAlertIcon } from "lucide-react";
 import{zodResolver}from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
@@ -10,8 +10,9 @@ import{Alert,AlertTitle}from "@/components/ui/alert";
 import { Form,FormControl,FormField,FormItem,FormLabel,FormMessage } from "@/components/ui/form";
 import { useForm } from "react-hook-form";
 import Link  from "next/link";
-import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useRouter } from "next/navigation";
+import{FaGithub,FaGoogle}from "react-icons/fa";
 
 const formSchema=z.object({
     email:z.string().email(),
@@ -38,13 +39,33 @@ export const SignInview=()=>{
             {
                 email:data.email,
                 password:data.password,
+                callbackURL:"/"
             },{
              onSuccess:()=>{
                 setPending(false);
                 router.push("/");
+                
              },
              onError:({error})=>{
                  setPending(false);
+                setError(error.message)
+             }     
+            }
+        )
+    }
+    const onSocial=(provider:'github'|'google')=>{
+        setError(null);
+        setPending(true);
+        authClient.signIn.social(
+            {
+                provider:provider,
+                callbackURL:"/"
+            },{
+             onSuccess:()=>{
+                setPending(false);
+             },
+             onError:({error})=>{
+                setPending(false);
                 setError(error.message)
              }     
             }
@@ -110,11 +131,11 @@ export const SignInview=()=>{
                                    </span>
                                 </div>
                                 <div className="grid grid-cols-2 gap-4">
-                                    <Button disabled={pending} variant="outline" type="button"className="w-full">
-                                       Google
+                                    <Button onClick={()=>onSocial("google")} disabled={pending} variant="outline" type="button"className="w-full">
+                                       <FaGoogle/>
                                     </Button>
-                                    <Button disabled={pending} variant="outline" type="button"className="w-full">
-                                       Github
+                                    <Button onClick={()=>onSocial("github")} disabled={pending} variant="outline" type="button"className="w-full">
+                                       <FaGithub/>
                                     </Button>
                                 </div>
                                 <div className="text-center text-sm">
